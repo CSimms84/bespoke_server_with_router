de683a94fcd536da4783a882e7f6245e7f915cb8
Charles Simms <30813081+CSimms84@users.noreply.github.com>
2024-05-29 08:17:41 -0400

    Add files via upload

---
+++ src/tcp_server.c
@@ -0,0 +1,57 @@
+#include <unistd.h>
+#include <stdio.h>
+#include <stdlib.h>
+#include <string.h>
+#include <arpa/inet.h>
+#include "../include/tcp_server.h"
+
+#define BUFFER_SIZE 1024
+
+void handle_tcp_client(int client_socket) {
+    char buffer[BUFFER_SIZE] = {0};
+    int bytes_read = read(client_socket, buffer, BUFFER_SIZE);
+    if (bytes_read > 0) {
+        printf("Received message: %s\n", buffer);
+        // Echo the message back to the client
+        write(client_socket, buffer, bytes_read);
+    }
+    close(client_socket);
+}
+
+void start_tcp_server(const char *port) {
+    int server_fd, client_socket;
+    struct sockaddr_in address;
+    int addrlen = sizeof(address);
+
+    if ((server_fd = socket(AF_INET, SOCK_STREAM, 0)) == 0) {
+        perror("socket failed");
+        exit(EXIT_FAILURE);
+    }
+
+    address.sin_family = AF_INET;
+    address.sin_addr.s_addr = INADDR_ANY;
+    address.sin_port = htons(atoi(port));
+
+    if (bind(server_fd, (struct sockaddr *)&address, sizeof(address)) < 0) {
+        perror("bind failed");
+        exit(EXIT_FAILURE);
+    }
+
+    if (listen(server_fd, 3) < 0) {
+        perror("listen failed");
+        exit(EXIT_FAILURE);
+    }
+
+    printf("TCP server listening on port %s\n", port);
+
+    while ((client_socket = accept(server_fd, (struct sockaddr *)&address, (socklen_t *)&addrlen)) >= 0) {
+        handle_tcp_client(client_socket);
+    }
+
+    if (client_socket < 0) {
+        perror("accept failed");
+        exit(EXIT_FAILURE);
+    }
+
+    close(server_fd);
+}
