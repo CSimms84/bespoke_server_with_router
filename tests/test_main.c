db29b0ca62eb15b2126bf0f69116d48d26380fbb
Charlie Simms <charlessimms84@gmail.com>
2024-06-01 20:55:34 -0400

    adding more unit tests

--- tests/test_main.c
+++ tests/test_main.c
@@ -2,6 +2,13 @@
 #include <CUnit/Basic.h>
 #include "router.h"
 #include "dns_server.h"
+#include <string.h>
+#include <unistd.h>
+#include <fcntl.h>
+#include <sys/types.h>
+#include <sys/socket.h>
+#include <netinet/in.h>
+#include <arpa/inet.h>
 
 // Forward declaration of functions to be tested
 void add_route(const char *path, route_handler_t handler);
@@ -10,23 +17,95 @@ void* run_dns_server(void* arg);
 
 // A dummy client socket implementation for testing
 void dummy_hello_handler(int client_socket) {
-    // Your dummy handler implementation for testing
+    const char *response = "HTTP/1.1 200 OK\r\nContent-Type: text/plain\r\n\r\nHello, World!";
+    write(client_socket, response, strlen(response));
 }
 
 // Test case for add_route
 void test_add_route() {
     add_route("/hello", dummy_hello_handler);
-    // Your assertions to verify the route was added
+
+    // Check that the route was added by simulating a request to "/hello"
+    int fds[2];
+    pipe(fds);
+    int client_socket = fds[1];
+
+    handle_request(client_socket);
+    close(client_socket);
+
+    char buffer[1024];
+    read(fds[0], buffer, sizeof(buffer));
+    close(fds[0]);
+
+    CU_ASSERT_STRING_EQUAL(buffer, "HTTP/1.1 200 OK\r\nContent-Type: text/plain\r\n\r\nHello, World!");
 }
 
 // Test case for handle_request
 void test_handle_request() {
-    // Set up a test case to simulate a request and check the response
+    // Set up a route and simulate a request to it
+    add_route("/test", dummy_hello_handler);
+
+    // Create a pipe to simulate client socket
+    int fds[2];
+    pipe(fds);
+    int client_socket = fds[1];
+
+    // Write a dummy HTTP request to the client socket
+    const char *request = "GET /test HTTP/1.1\r\nHost: localhost\r\n\r\n";
+    write(client_socket, request, strlen(request));
+    lseek(client_socket, 0, SEEK_SET);
+
+    handle_request(client_socket);
+    close(client_socket);
+
+    char buffer[1024];
+    read(fds[0], buffer, sizeof(buffer));
+    close(fds[0]);
+
+    CU_ASSERT_STRING_EQUAL(buffer, "HTTP/1.1 200 OK\r\nContent-Type: text/plain\r\n\r\nHello, World!");
 }
 
 // Test case for DNS server
 void test_dns_server() {
-    // Set up and run the DNS server, then test a DNS query
+    // Since the DNS server runs indefinitely, we test the handler instead
+    // Create a dummy DNS query
+    unsigned char query[] = {
+        0x12, 0x34, // ID
+        0x01, 0x00, // Flags (standard query)
+        0x00, 0x01, // QDCOUNT (1 question)
+        0x00, 0x00, // ANCOUNT
+        0x00, 0x00, // NSCOUNT
+        0x00, 0x00, // ARCOUNT
+        // Question section
+        0x07, 'e', 'x', 'a', 'm', 'p', 'l', 'e', 
+        0x03, 'c', 'o', 'm', 0x00, // QNAME (example.com)
+        0x00, 0x01, // QTYPE (A)
+        0x00, 0x01  // QCLASS (IN)
+    };
+    
+    // Buffer to hold the response
+    unsigned char response[512];
+
+    struct sockaddr_in client;
+    client.sin_family = AF_INET;
+    client.sin_addr.s_addr = inet_addr("127.0.0.1");
+    client.sin_port = htons(12345);
+
+    int sock = socket(AF_INET, SOCK_DGRAM, 0);
+    bind(sock, (struct sockaddr *)&client, sizeof(client));
+
+    struct sockaddr_in server;
+    socklen_t server_len = sizeof(server);
+
+    // Simulate receiving a DNS query
+    sendto(sock, query, sizeof(query), 0, (struct sockaddr *)&server, server_len);
+    recvfrom(sock, response, sizeof(response), 0, (struct sockaddr *)&server, &server_len);
+
+    // Check if the response is valid
+    CU_ASSERT(response[2] & 0x80); // QR bit should be set to 1 (response)
+    CU_ASSERT_EQUAL(response[3] & 0x0F, 0); // RCODE should be 0 (no error)
+
+    close(sock);
 }
 
 int main() {
